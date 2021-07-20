9efa74c698966c95841c96530875ee8dc403d86c
Michael Rittmeister <michael@rittmeister.in>
2021-07-20 19:26:42 +0200

    Fix some more CI

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# IntelliJ Gradle Updater
+
+##[2.0.2]
+
+### Added
+- Add CI
+### Changed
+- Bump dependencies
+### Fixed
+- Fix bug when trying to load settings file
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.changelog.markdownToHTML
+
 /*
  * MIT License
  *
@@ -22,16 +24,19 @@
  * SOFTWARE.
  */
 
+fun properties(key: String) = project.findProperty(key).toString()
+
 plugins {
     java
     kotlin("jvm") version "1.5.21"
     kotlin("plugin.serialization") version "1.5.21"
     id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
     id("org.jetbrains.intellij") version "1.1.3"
+    id("org.jetbrains.changelog") version "1.1.2"
 }
 
-group = "me.schlaubi"
-version = "2.0.2"
+group = properties("pluginGroup")
+version = properties("pluginVersion")
 
 repositories {
     mavenCentral()
@@ -57,29 +62,56 @@ tasks {
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version.set("2021.1.3")
-    plugins.set(
-        listOf(
-            // For gradle support
-            "gradle",
-            // To properly parse gradle-wrapper.properties
-            "properties",
-            // Some kotlin classes depend on the java plugin
-            "java",
-            // To properly parse build.gradle.kts
-            "org.jetbrains.kotlin"
-        )
-    )
+    pluginName.set(properties("pluginName"))
+    version.set(properties("platformVersion"))
+    type.set(properties("platformType"))
+    downloadSources.set(properties("platformDownloadSources").toBoolean())
+    updateSinceUntilBuild.set(true)
+
+    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
+    plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
+}
+
+changelog {
+    version = properties("pluginVersion")
+    groups = emptyList()
 }
 
 tasks {
     patchPluginXml {
-        changeNotes.set(
-            """
-            2.0.1
-            - Fix Gradle version fetching
-            """.trimIndent()
+        version.set(properties("pluginVersion"))
+        sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set(properties("pluginUntilBuild"))
+
+        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
+        pluginDescription.set(
+            File(projectDir, "README.md").readText().lines().run {
+                val start = "<!-- Plugin description -->"
+                val end = "<!-- Plugin description end -->"
+
+                if (!containsAll(listOf(start, end))) {
+
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end))
+            }.joinToString("\n").run { markdownToHTML(this) }
         )
+
+        // Get the latest available change notes from the changelog file
+        changeNotes.set(provider { changelog.getLatest().toHTML() })
+    }
+
+    runPluginVerifier {
+        ideVersions.set(properties("pluginVerifierIdeVersions").split(',').map(String::trim).filter(String::isNotEmpty))
+    }
+
+    publishPlugin {
+        dependsOn("patchChangelog")
+        token.set(System.getenv("PUBLISH_TOKEN"))
+        // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
+        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
+        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+        channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
     }
 }
 

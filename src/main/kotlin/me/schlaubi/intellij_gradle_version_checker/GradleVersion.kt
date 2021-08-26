c471a4c4628634438d28f2f0f47fa9adb7006510
Michael Rittmeister <michael@rittmeister.in>
2021-08-26 16:30:17 +0200

    Fix comparison of semver

--- src/main/kotlin/me/schlaubi/intellij_gradle_version_checker/GradleVersion.kt
+++ src/main/kotlin/me/schlaubi/intellij_gradle_version_checker/GradleVersion.kt
@@ -79,7 +79,7 @@ data class GradleVersion(val major: Int, val minor: Int, val revision: Int?) : C
         const val REVISION = 1
         const val MINOR = 2
         const val MAJOR = 3
-        const val TOO_NEW = 4
+        const val TOO_NEW = -4
     }
 
     override fun toString(): String = "$major.$minor${if (revision != null) ".$revision" else ""}"
